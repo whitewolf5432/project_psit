2efb59f0f20dcda13c6d92223919a44ca9405d00
Spicyz <bossthth1209mine@gmail.com>
2020-11-09 21:28:21 +0700

    make a canvas window

--- draft.py
+++ draft.py
@@ -4,7 +4,12 @@ import tkinter
 
 def main():
     """Main Function"""
+    height = 600
+    width = 800
     window = tkinter.Tk()
+    canvas = tkinter.Canvas(window, height=height, width=width)
+    canvas.pack()
+    window.mainloop()
 
 
 main()
