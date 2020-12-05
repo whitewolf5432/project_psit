f808e991832256526cb2f30d59cbb26ba96ae219
Spicyz <bossthth1209mine@gmail.com>
2020-12-06 01:22:25 +0700

    update bootup words

--- computies.py
+++ computies.py
@@ -1,5 +1,9 @@
 """Computies"""
 from os import system as sys
 from os import name
+from time import sleep as delay
+from src.bootup import bootup_words as bootup
 
-sys("echo %s" % name)
+sys("cls"), sys("title Computies") if name == "nt" else sys("clear"), sys("title Computies")
+
+for word in bootup: print(word); delay(0.3)
